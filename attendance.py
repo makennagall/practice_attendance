778b655eb233261e472f1e767b94644253368b5e
rnahm <rnahm@nd.edu>
2024-01-13 15:07:03 -0500

    sort, and handle empty values

--- attendance.py
+++ attendance.py
@@ -3,6 +3,7 @@
 import pygsheets 
 import pandas as pd
 from collections import defaultdict
+from math import isnan
 
 
 client = pygsheets.authorize(service_account_file='../attendance/key_2024') 
@@ -15,6 +16,7 @@ print(sheet_names)
 
 # opens a worksheet by its name/title 
 zero_worksht = raw_sheet.worksheet("title", sheet_names[0]) 
+one_worksht = raw_sheet.worksheet("title", sheet_names[1]) 
 
 
 data = zero_worksht.get_all_values()
@@ -28,13 +30,19 @@ nan_value = float("NaN")
  
 df.replace("", nan_value, inplace=True) 
 df.dropna(how='all', axis=1, inplace=True) 
-df = df.dropna()
+df.dropna(how='all', axis=0, inplace=True)
 columns = df.columns.to_list()
 print(columns)
 
+print(df.to_string())
 # create dict with all of the practices everybody has attended 
 values = defaultdict(set)
 for column in columns:
     names = df[column].to_list()
     for name in names:
+        if type(name) == float: continue
         values[name.lower()].add(column)
+
+# update values starting at A1
+combined_data = sorted([[names.title(), len(dates)] for names, dates in values.items()], key=lambda x : x[0].split(" ")[-1])
+one_worksht.update_values('A1', combined_data)
